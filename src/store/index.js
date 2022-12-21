7f28cdad872eb8be344955645b5ea61701e74ef7
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-12-21 09:30:42 +0200

    03 client side authentication

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {
+      id: null,
+      email: '',
+      username: ''
+    }
   },
   getters: {
   },
   mutations: {
+    CURRENT_USER_FETCHED(state, user) {
+      state.user.id = user.id
+      state.user.email = user.email
+      state.user.username = user.username
+    }
   },
   actions: {
+    async initialLoad(context) {
+      if (localStorage.bgtrackerjwt) { //checking for token
+        Vue.axios.defaults.headers.common.Authorization = `Bearer ${localStorage.bgtrackerjwt}` //setting authorization header for all requests
+        const res = await Vue.axios.get('/api/auth/currentUser') //making the request to get current user details
+        context.commit('CURRENT_USER_FETCHED', res.data.user) //save these details to our vuex
+      }
+    }
   },
   modules: {
   }
